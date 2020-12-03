ec89fa5615b97b184c659e6356a7a77ac5f1d386
nataliazour <nataliazour@hotmail.com>
2020-12-03 17:16:21 +0200

    initial commit

---
+++ 1_variables.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+
+    int i = 8;
+    char k = 'k';
+    float g = 5.7;
+    double l = 7.44525;
+
+    bool o = false;
+
+    printf("%d\n", i);
+    printf("%c\n", k);
+    printf("%f\n", g);
+    printf("%lf\n", l);
+    printf("%d\n", o);
+
+    return 0;
+}
---
+++ 3_basic_calculator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int A, B;
+    printf("Enter FIRST numbers: ");
+    scanf("%d", &A);
+    printf("Enter SECOND numbers: ");
+    scanf("%d", &B);
+    printf("Result: %d", A + B);
+}
